bf0783bf4ab32d7cfb5c028908347d331d130e30
liziwei01 <alssylk@gmail.com>
2022-03-03 15:37:34 +0800

    first-commit

---
+++ back-end/gin-idiary-appui/main.go
@@ -0,0 +1,12 @@
+/*
+ * @Author: liziwei01
+ * @Date: 2022-03-03 15:33:30
+ * @LastEditors: liziwei01
+ * @LastEditTime: 2022-03-03 15:33:35
+ * @Description: file content
+ */
+package main
+
+func main() {
+	
+}
