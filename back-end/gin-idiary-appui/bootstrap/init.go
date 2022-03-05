d846f15569fec29218f30bdfedabff76f565b7ab
liziwei01 <alssylk@gmail.com>
2022-03-05 15:18:36 +0800

    introduce gin middleware

---
+++ back-end/gin-idiary-appui/bootstrap/init.go
@@ -0,0 +1,38 @@
+/*
+ * @Author: liziwei01
+ * @Date: 2022-03-04 22:06:10
+ * @LastEditors: liziwei01
+ * @LastEditTime: 2022-03-05 00:24:59
+ * @Description: file content
+ */
+package bootstrap
+
+import (
+	"context"
+	"gin-idiary-appui/library/logit"
+	"gin-idiary-appui/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitMust(ctx context.Context) {
+	InitLog(ctx)
+	InitMiddleware(ctx)
+}
+
+func InitLog(ctx context.Context) {
+	logit.Init(ctx, "gin-idiary-appui")
+}
+
+func InitMiddleware(ctx context.Context) {
+	middleware.Init(ctx)
+}
+
+// InitHandler 获取http handler
+func InitHandler(app *AppServer) *gin.Engine {
+	gin.SetMode(app.Config.RunMode)
+	handler := gin.New()
+	// 注册log recover中间件
+	handler.Use(gin.Logger(), gin.Recovery())
+	return handler
+}
--- back-end/gin-idiary-appui/library/utils/base.go
+++ back-end/gin-idiary-appui/library/utils/base.go
@@ -2,17 +2,21 @@
  * @Author: liziwei01
  * @Date: 2022-03-03 19:52:24
  * @LastEditors: liziwei01
- * @LastEditTime: 2022-03-04 16:07:04
+ * @LastEditTime: 2022-03-04 23:52:40
  * @Description: 工具类分模块
  */
 package utils
 
 type (
-	USlice byte
+	USlice   byte
+	UConfig  byte
+	URequest byte
 )
 
 var (
-	Slice *USlice
+	Slice   *USlice
+	Config  *UConfig
+	Request *URequest
 )
 
 const ()
