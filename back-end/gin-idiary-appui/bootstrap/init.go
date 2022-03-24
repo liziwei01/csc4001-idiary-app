8d4b3532d6bb746510f245ade86f8348cae19c04
liziwei01 <alssylk@gmail.com>
2022-03-25 00:01:50 +0800

    introduce log middleware

--- back-end/gin-idiary-appui/bootstrap/init.go
+++ back-end/gin-idiary-appui/bootstrap/init.go
@@ -2,7 +2,7 @@
  * @Author: liziwei01
  * @Date: 2022-03-04 22:06:10
  * @LastEditors: liziwei01
- * @LastEditTime: 2022-03-10 20:39:09
+ * @LastEditTime: 2022-03-24 23:43:27
  * @Description: file content
  */
 package bootstrap
@@ -37,6 +37,8 @@ func InitHandler(app *AppServer) *gin.Engine {
 	gin.SetMode(app.Config.RunMode)
 	handler := gin.New()
 	// 注册log recover中间件
-	handler.Use(gin.Logger(), gin.Recovery())
+	ginRecovery := gin.Recovery()
+	baiduLogger := logit.LogitMiddleware()
+	handler.Use(ginRecovery, baiduLogger)
 	return handler
 }
