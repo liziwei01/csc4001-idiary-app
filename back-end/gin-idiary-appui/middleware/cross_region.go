5b1b5e73245351484c5eccbb7ed8daa9dd61a4ce
liziwei01 <alssylk@gmail.com>
2022-04-12 22:36:10 +0800

    fix cross region bug 2

--- back-end/gin-idiary-appui/middleware/cross_region.go
+++ back-end/gin-idiary-appui/middleware/cross_region.go
@@ -2,16 +2,26 @@
  * @Author: liziwei01
  * @Date: 2022-04-12 21:54:39
  * @LastEditors: liziwei01
- * @LastEditTime: 2022-04-12 21:55:41
+ * @LastEditTime: 2022-04-12 22:30:14
  * @Description: file content
  */
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CrossRegionMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
+		c.Header("Access-Control-Max-Age", "1728000")
+		if c.Request.Method == "OPTIONS" {
+			c.JSON(http.StatusOK, "ok")
+		}
 		c.Next()
 	}
 }
