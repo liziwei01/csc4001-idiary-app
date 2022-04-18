76cca107a2a3b6bcb17e54e408a4e9b40cebc062
yikerui <118010386@link.cuhk.edu.cn>
2022-04-18 16:18:06 +0800

    combine

---
+++ front-end/idiary-app/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+// 设置代理配置
+module.exports = function(app) {
+    app.use(
+        createProxyMiddleware("/api", {
+            target: "http://localhost:8080/", //服务器接口地址 // http://120.78.134.104:8080/api/upload/image
+            changeOrigin: true,
+            secure: false,
+        })
+    );
+};
